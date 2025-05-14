0bde87c4c4703907b2c05e3c6152142a52263a7b
Poschinski <marcus.unglert@web.de>
2025-05-14 12:42:37 +0200

    add env variable for socket server

--- client/app/socket.ts
+++ client/app/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001', { autoConnect: false });
+const socket = io(import.meta.env.VITE_SOCKET_HOST, { autoConnect: false });
 
 export default socket;
