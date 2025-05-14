5e27dbdd7880ba576449d283458ff1421be32a26
Poschinski <marcus.unglert@web.de>
2025-05-14 12:41:56 +0200

    Add Dockerfile and update CORS settings in index.js; add package.json scripts for production

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const randomId = () => crypto.randomBytes(8).toString("hex");
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://ddf.poschinski.de"],
     methods: ["GET", "POST"],
   },
 });
