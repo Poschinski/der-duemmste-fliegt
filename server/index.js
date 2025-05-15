9d6b54a9eed79621da2b63c947609b296f4d1db3
Poschinski <marcus.unglert@web.de>
2025-05-15 09:12:35 +0200

    add transport policies to cors

--- server/index.js
+++ server/index.js
@@ -19,6 +19,7 @@ const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "http://localhost:3000", "https://ddf.poschinski.de"],
     methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
   },
 });
 
