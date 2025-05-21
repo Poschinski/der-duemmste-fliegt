41dbf194508ca16fa31194eba14ac7d94c015bb4
Poschinski <marcus.unglert@web.de>
2025-05-21 17:35:48 +0200

    add transport config to socket connection

--- client/app/socket.ts
+++ client/app/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io(import.meta.env.VITE_SOCKET_HOST, { autoConnect: false,  path: '/socket.io', withCredentials: true });
+const socket = io(import.meta.env.VITE_SOCKET_HOST, { autoConnect: false,  path: '/socket.io', withCredentials: true, transports: ['websocket'] });
 
 export default socket;
