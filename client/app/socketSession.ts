270f9bb995472f4adf22735e84e5526a5e7d3052
Poschinski <marcus.unglert@web.de>
2025-05-22 10:16:06 +0200

    refactor socket session initialization for clarity and consistency

--- client/app/socketSession.ts
+++ client/app/socketSession.ts
@@ -1,27 +1,31 @@
 import socket from "./socket";
 
 const initSocketSession = (lobbyID: string, isModerator?: boolean, name?: string) => {
-    console.log(lobbyID)
+  if (socket.connected) socket.disconnect();
+
   const sessionID = sessionStorage.getItem("sessionID");
   if (sessionID) {
     socket.auth = { sessionID };
-    socket.connect();
   } else {
-    if (isModerator) {
-      socket.auth = {
-        username: name,
-        isMod: true,
-        lobbyID: lobbyID,
-      };
-    } else {
-      socket.auth = { username: name, isMod: false, lobbyID: lobbyID };
-    }
-    socket.connect();
+    socket.auth = {
+      username: name,
+      isMod: !!isModerator,
+      lobbyID: lobbyID,
+    };
   }
+
+  socket.connect();
 
   socket.on("session", ({ sessionID }) => {
     socket.auth = { sessionID };
     sessionStorage.setItem("sessionID", sessionID);
+  });
+
+  socket.on("connect_error", (err) => {
+    if (err?.message === "Session ID unknown") {
+      sessionStorage.removeItem("sessionID");
+      window.location.reload();
+    }
   });
 };
 
