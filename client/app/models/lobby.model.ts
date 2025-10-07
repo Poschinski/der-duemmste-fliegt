cd2951fed638697c4e0085bb12f660bfb98d2164
Poschinski <marcus.unglert@web.de>
2025-10-07 22:57:24 +0200

    update some fields

--- client/app/models/lobby.model.ts
+++ client/app/models/lobby.model.ts
@@ -2,7 +2,7 @@ export type UserRole = "moderator" | "player";
 
 export interface LobbyUser {
   id: string;
-  socketId: string;
+  socketId?: string;
   role: UserRole;
   name?: string;   // Falls Client noch Name mitliefert
   lives?: number;
@@ -10,19 +10,21 @@ export interface LobbyUser {
 
 export type LobbyPhase = "waiting" | "question" | "voting" | "result";
 
+export interface Settings {
+  maxLives: number;
+  roundTime: number; 
+}
+
 export interface Lobby {
-  id: string;
-  moderatorId: string;
-  settings: {
-    maxLives: number;
-    roundTime: number;
-  };
-  users: Record<string, LobbyUser>;
-  phase: LobbyPhase;
-  currentQuestion: string | null;
-  currentCorrectAnswer: string | null;
-  currentAnswers: any[]; // genauer typisieren, wenn klar
-  usedQuestions: string[];
-  votes: Record<string, string>; // voterId -> targetId
-  timer: number | null;
+  id?: string;
+  moderatorId?: string;
+  settings?: Settings
+  users?: Record<string, LobbyUser>;
+  phase?: LobbyPhase;
+  currentQuestion?: string | null;
+  currentCorrectAnswer?: string | null;
+  currentAnswers?: any[]; // genauer typisieren, wenn klar
+  usedQuestions?: string[];
+  votes?: Record<string, string>; // voterId -> targetId
+  timer?: number | null;
 }
